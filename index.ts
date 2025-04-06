6ec12a920e8d8b1816b552ef867a89c7cd834cf5
DarkGuy10 <62807269+DarkGuy10@users.noreply.github.com>
2025-04-06 17:59:35 +0530

    .

--- index.ts
+++ index.ts
@@ -1,14 +1,15 @@
 Bun.serve({
-  port: 3000,
-  routes: {
-    '/': new Response('OK') ,
-    '/nya': req => {
-      const url = new URL(req.url)
-      const redirect = url.searchParams.get('redirect')
-      if(!redirect) 
-        return new Response('Missing redirect')
-      else
-        return Response.redirect(redirect)
-    }
-  }
+	port: 3000,
+	routes: {
+		'/': (req: Request) => {
+			console.log(req.headers)
+			return new Response('OK')
+		},
+		'/nya': req => {
+			const url = new URL(req.url)
+			const redirect = url.searchParams.get('redirect')
+			if (!redirect) return new Response('Missing redirect')
+			else return Response.redirect(redirect)
+		},
+	},
 })
