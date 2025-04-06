866656344d8afb2b1c0f43d7f363ed55663ac8a1
DarkGuy10 <62807269+DarkGuy10@users.noreply.github.com>
2025-04-06 18:09:40 +0530

    .

--- index.ts
+++ index.ts
@@ -5,6 +5,14 @@ Bun.serve({
 			console.log(req.headers)
 			return new Response('OK')
 		},
+		'/sample.css': async (req: Request) => {
+			console.log(req.headers)
+			return new Response(await Bun.file('./sample.css').bytes(), {
+				headers: {
+					'Content-Type': 'text/css',
+				},
+			})
+		},
 		'/nya': req => {
 			const url = new URL(req.url)
 			const redirect = url.searchParams.get('redirect')
